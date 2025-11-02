564cdbde318d51e74916015f290512ab127686da
Kyuuto09 <cselfymain@gmail.com>
2025-11-02 14:45:37 +0100

    frontend: welcome section buttons added, significantly improved css + index.css structure

---
+++ frontend/src/components/LogIn/Login.jsx
@@ -0,0 +1,19 @@
+import './Login.css';
+/*
+ * МІНІМАЛЬНИЙ ШАБЛОN (Login.jsx)
+ *
+ * Пояснення:
+ * 1. Створюй розмітку (HTML/JSX) тут.
+ * 2. Давай елементам семантичні 'className' (напр., "login-title").
+ * 3. Переходь в 'Login.css', щоб стилізувати ці класи.
+ */
+const Login = () => {
+  return (
+    // Можеш додати 'login-page-wrapper' для центрування
+    <div>
+      <h1 className="login-title">Log in</h1>
+    </div>
+  );
+};
+
+export default Login;
