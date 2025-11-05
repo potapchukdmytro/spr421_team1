811ee59d2480db635f82c7647f3135e83bc9feb9
da2045 <vitde2009@gmail.com>
2025-11-05 19:16:11 +0200

    add login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css'
+import Login from './components/LogIn/Login'
 import Welcome from './components/Welcome/Welcome'
+import { Routes, Route, Link } from "react-router-dom"
+
 
 const App = () => {
   return (
     <div className="app">
-      <Welcome />
+      <Routes>
+        <Route path='/' element={<Welcome/>} />
+        <Route path='/login' element={<Login/>} />
+      </Routes>
     </div>
   )
 }
--- frontend/src/components/LogIn/Login.jsx
+++ frontend/src/components/LogIn/Login.jsx
@@ -1,4 +1,5 @@
 import './Login.css';
+import { Link } from "react-router-dom";
 /*
  * МІНІМАЛЬНИЙ ШАБЛОN (Login.jsx)
  *
@@ -11,7 +12,26 @@ const Login = () => {
   return (
     // Можеш додати 'login-page-wrapper' для центрування
     <div>
+      
       <h1 className="login-title">Log in</h1>
+      <div className='input_div'>
+          <h2 className='subtitle_1'>
+              Email
+          </h2>
+          <input className='email_input' placeholder="name@example.com"></input>
+        </div>
+      <div className='input_div2'>
+        <h2 className='subtitle_2'>
+              Password
+          </h2>
+          <input type="password"  className='email_input'></input>
+      </div>
+      <div className='button_conteiner'>
+      <button className='button_log'>Log In</button>
+      </div >
+      <p className='registr_acc'>Don`t have an account?
+        <Link to="/register" className='credits'>Sign Up </Link>
+      </p>
     </div>
   );
 };
