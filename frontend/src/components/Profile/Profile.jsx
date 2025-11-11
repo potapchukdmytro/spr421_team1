cacb6a6eeb76325960b46e02a9c7e89ead9d1e36
Kyuuto09 <cselfymain@gmail.com>
2025-11-11 11:42:23 +0100

    (frontend) Settings & Profile

---
+++ frontend/src/components/Profile/Profile.jsx
@@ -0,0 +1,17 @@
+﻿import { useNavigate } from 'react-router-dom'
+import './Profile.css'
+
+const Profile = () => {
+  const navigate = useNavigate()
+
+  return (
+    <div className="settings-overlay" onClick={(e) => e.target === e.currentTarget && navigate('/chat')}>
+      <div className="settings-modal">
+        <h2 style={{color: 'black', padding: '20px'}}>Settings Modal Works!</h2>
+        <button onClick={() => navigate('/chat')} style={{padding: '10px', margin: '20px'}}>Close</button>
+      </div>
+    </div>
+  )
+}
+
+export default Profile
