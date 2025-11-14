9db96f1b78ad3a8a57e6ad8a4de62893cc27ccf3
axneo27 <astrofymchuk@gmail.com>
2025-11-14 22:20:10 +0200

    h3

--- backend/web_chat/Program.cs
+++ backend/web_chat/Program.cs
@@ -105,7 +105,7 @@ builder.Services.AddCors(options =>
 // DbContext - THIS IS THE CORRECT, SINGLE ENTRY
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
-    options.UseNpgsql(builder.Configuration.GetConnectionString("hosted_db2"));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("hosted_db3"));
 });
 
 builder.Services.AddEndpointsApiExplorer();
